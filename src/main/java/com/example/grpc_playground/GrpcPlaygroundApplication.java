5f35368d9235d63ec6a0afa836f5def1c8c019f1
RUSHABH82 <59302177+RUSHABH82@users.noreply.github.com>
2025-10-11 19:24:31 +0530

    Intital Springboot comment

---
+++ src/main/java/com/example/grpc_playground/GrpcPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpc_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcPlaygroundApplication.class, args);
+	}
+
+}
